679047c12ee06d000c14baa8223dae323b094eb6
Pankaj Jindal <pankajjindal@LucidMotors.com>
2022-01-04 15:41:21 -0800

    test1

--- java/StackUse.java
+++ java/StackUse.java
@@ -1,34 +1,50 @@
 
 class Stack{
 
-	int[] stck;
-	int tos = -1;
+	private int[] stck = new int[10];
+	private int tos = -1;
+
+	Stack(){
+		tos = -1;
+	}
 
 void push(int x){
-  	if ( tos >= 10)
+  	if ( tos >= 9 )
      		System.out.println(" underflow");
   	else
-     		stck[tos++]=x;
+     		stck[++tos]=x;
 }
 	
 
 
 int pop()
 {
- 	if (tos > -1)
+ 	if (tos >= 0)
      		return stck[tos--];
  	else {
      		System.out.println(" underflow");
-		return 0;
+			return 0;
 	}
 }
 
 }
 
 public class StackUse{
-  	public static void main()
+  	public static void main(String[] args)
 	{
      		System.out.println(" Inside main");
+			Stack s1 = new Stack();
+			
+			s1.push(1);
+			s1.push(2);
+			s1.push(4);
+			s1.push(3);
+			System.out.println("Pop returned :"+s1.pop());
+			System.out.println("Pop returned :"+s1.pop());
+			System.out.println("Pop returned :"+s1.pop());
+			System.out.println("Pop returned :"+s1.pop());
+			System.out.println("Pop returned :"+s1.pop());
+
 
 	}
 }
